280042a647b08ca1a101a57e8279800c2cff5189
bigsu <workszj@126.com>
2024-08-23 10:50:06 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,8 +4,8 @@ import { set_sidebar } from "../utils/auto_sidebar.mjs";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/API-Docs/',
-  title: "元宇宙服务接口文档",
-  description: "元宇宙服务接口文档",
+  title: "数据服务接口文档",
+  description: "数据服务接口文档",
   head: [['link', { rel: 'icon', href: '/API-Docs/images/logo.png' }]],
   lang: 'zh-CN',
   // 是否显示更新时间
@@ -16,12 +16,34 @@ export default defineConfig({
     logo: { light: '/images/logo.svg', dark: '/images/logo_dark.svg', alt: '' },
     lightModeSwitchTitle: '切换到浅色模式',
     darkModeSwitchTitle: '切换到深色模式',
+    darkModeSwitchLabel: '外观',
+    returnToTopLabel: '返回顶部',
     docFooter: {
       prev: '上一篇',
       next: '下一篇'
     },
     lastUpdatedText: "最后更新",
     outlineTitle: "文章目录",
+    sidebarMenuLabel: '菜单',
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换'
+            }
+          }
+        }
+      }
+    },
     nav: [
       { text: '首页', link: '/index' },
       { text: '指南', link: '/docs/简介' }
